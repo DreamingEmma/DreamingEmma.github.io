05806269c4c5540e790499f80286d78d04e81c5b
DreamingEmma <dreamingemma1@gmail.com>
2015-09-24 19:46:54 -0400

    Changed game over text, minor text changes on page

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -156,7 +156,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "Go deeper now." : "Drop now.";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
