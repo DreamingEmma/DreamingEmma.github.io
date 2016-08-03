74458f0d93a3d3269eb39e73bfda1ba4127367f9
DreamingEmma <dreamingemma1@gmail.com>
2016-08-03 16:51:06 -0400

    Tweaked game end messages

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -156,7 +156,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "Go deeper now." : "Drop now.";
+  var message = won ? "Deeper and deeper." : "You can always fall deeper.";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
